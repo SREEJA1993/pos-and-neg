5b182132003bbc457cb1c4dcf65949680e4517de
SREEJA1993 <sreeja9390@gmail.com>
2017-06-08 11:15:22 +0530

    Create vov.c

---
+++ vov.c
@@ -0,0 +1,13 @@
+  #include<stdio.h>
+  #include<conio.h>
+  #include<string.h>
+  int main()
+  {
+  char n;
+  scanf("%c",&n);
+  if(n=='a'||'e'||'i'||'o'||'u')
+  printf("vowel");
+  else
+  pritnf("consonent");
+  return 0;
+  }
