9fa2d3539596d3ba69ff892cf3dabbc20fa98891
SREEJA1993 <sreeja9390@gmail.com>
2017-06-27 15:58:55 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+     int a,i,c;
+     scanf("%d",&a);
+     for(i=1;i<=a;i++)
+     {
+         if(i%2==1)
+         {
+             printf("%d\t",i);
+         }
+     }
+    
+
+    return 0;
+}
